92dd0f6da6b933991e7fafd5c0f899fd628c1ff5
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-12 19:36:17 -0700

    co

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
