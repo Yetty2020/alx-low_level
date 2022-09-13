073926727fe2a543647eeef7e6152c03291dfcf4
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-12 17:29:04 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the string in the pur function
+ * this program prints "Programming is line building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
