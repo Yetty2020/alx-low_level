d96118d67c05ab0e5338100b0947834906bfd83b
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-17 06:41:39 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
- * main - test function that prints if integr is posiive or negative
+ * main - prints positive or negative integer
  * Return: 0
  */
 int main(void)
 {
 	int i;
+	i = 0;
 
-	i =0;
-	postive_or_negative(i);
-
+	positive_or_negative(i);
 	return (0);
 }
