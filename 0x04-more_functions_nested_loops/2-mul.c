6a3142a2857d9e5881ae37ffaa3ec8694227162c
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-16 17:17:04 -0700

    co

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * mul - multiplies two integers
- * @a - int to e multiplies to b
- * @b - int to be multioplies to a
- * Return: the result of the operation
+ *mul - multiplies two integers
+ *@a - int to e multiplies to b
+ *@b - int to be multioplies to a
+ *Return: the result of the operation
  */
 int mul(int a, int b)
 {
