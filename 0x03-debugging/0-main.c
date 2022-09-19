98d033ad4d7d1f5546fb3ba381fa06dd434fa912
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-19 06:03:51 -0700

    co

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * main - prints positive or negative integer
- * Return: 0
+ * Return: 0i
  */
 int main(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
