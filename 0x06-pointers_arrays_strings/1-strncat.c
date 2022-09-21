a7b7ae20396987d53369f05d80d728c5e49459ea
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-21 03:55:13 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checkk the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *dest[12] = "Hello";
+	char *src[n] = "World";
+	int n = 12;
+
+	_strncat(*dest, *src);
+	printf("_strncat(*dest, *src) : %s\n", *dest);
+	return (0);
+
+}
