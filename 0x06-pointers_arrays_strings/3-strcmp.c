99281eb59c40b29d1374c5429665333ad9fc3050
Yetty2020 <fatihaadetoro@gmail.com>
2022-09-22 14:47:10 -0700

    commut

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,12 +3,14 @@
  * _strcmp - Function which compare two strings
  * @s1: first string
  * @s2:second string
- * Return: returns zero if s1 == s2
+ * Return:
+ * 		returns zero if s1 == s2
+ * 		returns negative number if s1 < s2
+ * 		returns positive number if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	diff = 0;
+	int i = 0, diff = 0;
 
 	while(1)
 	{
